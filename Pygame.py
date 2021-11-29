1d62adcfabdef443f66356860ea982d1e3127e03
Shyan Huff <70677678+s11142841@users.noreply.github.com>
2021-11-29 09:09:35 -0500

    0.1
    Imported pygame and window surface

---
+++ Pygame.py
@@ -0,0 +1,10 @@
+# shyan huff, 11/29/21
+
+import pygame, sys 
+from pygame.locals import *
+
+# start PyGame 
+windowSurface = pygame.display.set_mode( (500,400)0, 32)
+pygame.display.set_caption("Hello, world!")
+# Set Color Values
+Black = (0, 0, 0)
