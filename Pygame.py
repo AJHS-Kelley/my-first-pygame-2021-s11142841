eeb5ad91d875d2e84c2a666684069f7a35436f6f
Shyan Huff <70677678+s11142841@users.noreply.github.com>
2021-11-29 09:17:33 -0500

    0.2
    Imported color values

--- Pygame.py
+++ Pygame.py
@@ -4,7 +4,21 @@ import pygame, sys
 from pygame.locals import *
 
 # start PyGame 
+pygame.init() 
+# Create Game Window
 windowSurface = pygame.display.set_mode( (500,400)0, 32)
 pygame.display.set_caption("Hello, world!")
+
 # Set Color Values
-Black = (0, 0, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# Setup Fonts 
+basicFont = pygame.font.SysFont(None, 48)
+
+# Setup Text
+text = basicFont.render('Hello World!')
+
